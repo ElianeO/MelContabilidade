78c8aad3ee17a17555ec861c1f897850c28e3f7f
ElianeO <elianecristina_d@yahoo.com>
2023-04-26 14:49:08 -0300

    "(26/04) Correções na tabela usuários"

--- config/config.js
+++ config/config.js
@@ -10,27 +10,5 @@ module.exports = {
       "underscored": true,
       "underscoredAll": true
     } */
-  },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    /*"define": {
-      "underscored": true,
-      "underscoredAll": true
-    } */
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    /*"define": {
-      "underscored": true,
-      "underscoredAll": true
-    }*/
   }
 }
